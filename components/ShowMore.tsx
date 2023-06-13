fb729d016235b5bbe6d5d8933416a447bab83a36
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-06-13 11:46:02 +0300

    bug fix with scroll on top with next 13.4

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,14 +5,12 @@ import { updateSearchParams } from "@/utils";
 import { useRouter } from "next/navigation";
 import CustomButton from "./CustomButton";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const router = useRouter();
 
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathname = updateSearchParams("limit", `${newLimit}`);
-
-    router.push(newPathname);
+    setLimit(newLimit);
   };
   return (
     <div className="w-full flex-center gap-5 mt-10">
